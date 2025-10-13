ef1a4bd322d444e7640284a20a9aaa8c4f544508
xyzarnav <arnav20604@gmail.com>
2025-10-13 20:30:40 +0530

    fix: correct backend start script to use 'dev' command for proper execution

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  outputFileTracingRoot: require('path').join(__dirname, '..'),
 };
 
 export default nextConfig;
